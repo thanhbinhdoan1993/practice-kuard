273a328f71695b1c1663db63741d39288b3f1786
Binh Doan <thanhbinhdoan1993@gmail.com>
2020-08-28 20:22:21 +0700

    add new code

--- client/src/keygen.jsx
+++ client/src/keygen.jsx
@@ -36,3 +36,124 @@ const schema = {
         }
     }
 };
+
+const uiSchema = {
+    enable: {
+        classNames: "foo"
+    }
+}
+
+export default class KeyGen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            config: {
+                enable: false,
+                numToGen: 0,
+                timeToRun: 0,
+                exitOnComplete: false,
+                exitCode: 0
+            },
+            history: []
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    loadState(initial) {
+        fetch(this.props.serverPath)
+            .then(fetchError)
+            .then(response => response.json())
+            .then(response => {
+                if (initial) {
+                    this.setState(response)
+                } else {
+                    this.setState(state => {
+                        state.history = response.history
+                        return state
+                    })
+                }
+            })
+            .catch(err => this.context.reportConnError());
+    }
+
+    componentDidMount() {
+        this.loadState(true)
+        this.timer = setInterval(this.loadState.bind(this, false), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    handleSubmit(event) {
+        let payload = JSON.stringify(this.state.config)
+        fetch(this.props.serverPath, {
+            method: "PUT",
+            body: payload
+        })
+            .then(fetchError)
+            .then(response => response.json())
+            .then(response => this.setState(previousState => {
+                previousState = response
+                return previousState
+            }))
+            .catch(err => this.context.reportConnError());
+    }
+
+    handleChange({ formData }) {
+        this.setState(state => {
+            state.config = formData;
+            return state
+        })
+    }
+
+    render() {
+        let history = <p>No recorded workload history</p>
+        if (this.state.history.length > 0) {
+            let historyItems = [];
+            for (let h of this.state.history) {
+                historyItems.push(<span key={h.id}>{h.data}{"\n"}</span>)
+            }
+            historyItems.reverse()
+            history = (<pre>{historyItems}</pre>)
+        }
+
+        return (
+            <div>
+                <div className="panel panel-default">
+                    <div className="panel-heading">KeyGen Synthetic Workload</div>
+                    <div className="panel-body">
+                        <div>This controls a synthetic workload on the server: creating 4096
+                        bit RSA key pairs. These parameters control how many to create
+                        and, optionally, cause the server to exit with a specific exit
+                        code.
+                        </div>
+                        <Form
+                            schema={schema}
+                            uiSchema={uiSchema}
+                            className="form"
+                            formData={this.state.config}
+                            onChange={this.handleChange}
+                            onSubmit={this.handleSubmit}>
+                            <input
+                                className="btn btn-default"
+                                type="submit"
+                                value="Submit" />
+                        </Form>
+                    </div>
+                </div>
+                {history}
+            </div>
+        )
+    }
+}
+
+KeyGen.propTypes = {
+    serverPath: React.PropTypes.string.isRequired,
+}
+
+KeyGen.contextType = {
+    reportConnError: React.PropTypes.func
+}
